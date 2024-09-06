8753eda8ad572f1cbca383e89a4714b153b23727
Wellington Delmondes <92116516+1993nxv@users.noreply.github.com>
2024-09-05 22:10:08 -0300

    Criando classe AuthorizationServerConfig.

---
+++ src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.algaworks.algafood.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("delmondes")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN")
+			.and()
+			.withUser("bruna")
+				.password(passwordEncoder().encode("321"))
+				.roles("GERENTE");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.httpBasic()
+			.and()
+			.authorizeRequests()
+				.antMatchers("/restaurantes/**").permitAll()
+				.anyRequest().authenticated()
+//		Retirando o uso de sessions/cookies
+		.and()
+			.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//		Retirando o uso de csrf		
+		.and()
+			.csrf().disable();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
