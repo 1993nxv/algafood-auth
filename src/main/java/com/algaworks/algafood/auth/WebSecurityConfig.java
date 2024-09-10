d945bd2b6e6b70e7eef0513405daff7f8f16edd8
Delmondes <wellington.apk@gmail.com>
2024-09-10 20:15:25 -0300

    Configurando o fluxo Authorization Server com Password Credentials e Opaque Tokens.

--- src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -33,9 +34,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.antMatchers("/restaurantes/**").permitAll()
 				.anyRequest().authenticated()
+//		Retirando o uso de sessions/cookies
 		.and()
 			.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//		Retirando o uso de csrf		
 		.and()
 			.csrf().disable();
 	}
@@ -43,5 +46,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
 	}
 }
