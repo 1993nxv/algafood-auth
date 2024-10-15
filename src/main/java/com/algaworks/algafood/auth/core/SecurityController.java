ddfce0a680a3794ba7108e7a52fd4e233df77c8e
Delmondes <wellington.apk@gmail.com>
2024-10-14 23:17:37 -0300

    Customizando a página de login.

---
+++ src/main/java/com/algaworks/algafood/auth/core/SecurityController.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.auth.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
--- src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
+++ src/main/java/com/algaworks/algafood/auth/core/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.auth.core;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,17 +13,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
-//	@Override
-//	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//		auth.inMemoryAuthentication()
-//			.withUser("delmondes")
-//				.password(passwordEncoder().encode("123"))
-//				.roles("ADMIN")
-//			.and()
-//			.withUser("bruna")
-//				.password(passwordEncoder().encode("321"))
-//				.roles("GERENTE");
-//	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.formLogin().loginPage("/login")
+			.and()
+				.authorizeRequests()
+				.antMatchers("/oauth/**").authenticated();
+	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
